2eb73569e20802e35fc456c1d9a45f01321cf930
Paulo <paulozamora2000@gmail.com>
2023-03-26 15:07:03 -0600

    Create svelte project

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { onMount } from "svelte";
+    import axios from 'axios';
+    import demoJSON from '../../demo.json'
+
+    let devices = {}
+    let connections = {}
+
+
+    onMount(async () => {
+        console.log("hello")
+        devices = demoJSON.devices;
+        connections = demoJSON.connections;
+        // const res = await axios.get("localhost:8000");
+        // console.log(res);
+    })
+
+
+</script>
+
+<svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+    <div class="main-container">
+        hello world
+    d
+    </div>
+</section>
+
+<style>
+    .main-container{ 
+        background: white;
+        width: 60vw;
+        height: 95vh;
+        border-radius: 10px;
+    }
+</style>
